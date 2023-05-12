1775d1ba78cd9e982dfa5d930b878d04a8ae3e88
Ronald Albert <ronaldalbert1609@gmail.com>
2023-05-11 23:26:01 -0300

    Update Lista1

--- src/Lista1.jl
+++ src/Lista1.jl
@@ -17,27 +17,32 @@ module Lista1
         X_positive = dataset.X[findall(y -> y == 1, dataset.y)]
         X_negative = dataset.X[findall(y -> y != 1, dataset.y)]
 
+        # Plotando os pontos de treino com y = 1
         x_1_positive = [x[1] for x in X_positive]
         x_2_positive = [x[2] for x in X_positive]
         plot(x_1_positive, x_2_positive, label="Dados de Treino (y = 1)", seriestype=:scatter, color=:lightgreen)
 
+        # Plotando os pontos de treino com y = -1
         x_1_negative = [x[1] for x in X_negative]
         x_2_negative = [x[2] for x in X_negative]
-        plot!(x_1_negative, x_2_negative, label="Dados de Treino (y = 0)", seriestype=:scatter, color=:indianred)
+        plot!(x_1_negative, x_2_negative, label="Dados de Treino (y = -1)", seriestype=:scatter, color=:indianred)
 
         X_test_positive = dataset.X_test[findall(y -> y == 1, dataset.y_test)]
         X_test_negative = dataset.X_test[findall(y -> y != 1, dataset.y_test)]
 
+        # Plotando os pontos de teste comm y = 1
         x_1_positive_test = [x[1] for x in X_test_positive]
         x_2_positive_test = [x[2] for x in X_test_positive]
         plot!(x_1_positive_test, x_2_positive_test, label="Dados de Teste (y = 1)", seriestype=:scatter, color=:green)
 
+        # Plotando os pontos de teste comm y = -1
         x_1_negative_test = [x[1] for x in X_test_negative]
         x_2_negative_test = [x[2] for x in X_test_negative]
-        plot!(x_1_negative_test, x_2_negative_test, label="Dados de Teste (y = 0)", seriestype=:scatter, color=:red)
+        plot!(x_1_negative_test, x_2_negative_test, label="Dados de Teste (y = -1)", seriestype=:scatter, color=:red)
 
         fit!(model, dataset.X, dataset.y)
 
+        # Plotando a função objetivo e a função encontrada pelo modelo
         x = [-1, 1]
         f = (x -> -dataset.w[1]*x/dataset.w[2] - dataset.b/dataset.w[2])
         g = (x -> -model.w[1]*x/model.w[2]- model.b/model.w[2])
